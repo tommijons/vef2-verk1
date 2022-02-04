6873f37822966e9c4e199feeed365629016c2cf8
tommijons <toj6@hi.is>
2022-02-04 19:06:07 +0000

    test

--- src/make-html.js
+++ src/make-html.js
@@ -33,7 +33,7 @@ export function dataTemplate(title, data, showBack = false) {
   <html>
     <head>
       <title>${title ?? ''}</title>
-      <link rel="stylesheet" href=styles.css">
+      <link rel="stylesheet" href="../public/styles.css">
     </head>
     <body>
       ${data ?? ''}
